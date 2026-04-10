3161aaec4b55d744dd20ae2540e0da50993141ff
dependablediamondtransport-lab <dependablediamondtransport@gmail.com>
2026-04-11 01:14:24 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -102,6 +102,10 @@ app.post("/create-checkout", async (req, res) => {
   }
 });
 
+app.get("/health", (_req, res) => {
+  res.json({ ok: true });
+});
+
 app.listen(3000, () => {
   console.log("Server running on http://localhost:3000");
 });
